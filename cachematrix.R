51970ec0dceb69f7b3b7940b49a0957ae04bb88f
Ramesh Allada <rallada9@gmail.com>
2015-07-25 10:33:37 -0500

    RA CacheMarrix Program Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,40 @@
 ## functions do
 
 ## Write a short comment describing this function
+# This function is used to create a inverse matrix by using the 'solve'
+# function operation. This function also stores the calculated inverse matrix for
+# use in other functions. 
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	set <- function(y) {
+		m <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setmatrix <- function(solve) m <<- solve
+	getmatrix <- function() m
+	list(set = set, get = get,
+		setmatrix = setmatrix,
+		getmatrix = getmatrix)
+
 
 }
 
 
 ## Write a short comment describing this function
+# This function calls the makeCachMaster function. It checks if the inverse has # been calculated and if it has it will get this cached value else it callculates
+# the inverse of a matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getmatrix()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+		}
+		data <-$get()
+		m <- solve(data, ...)
+		x$setmatrix(m)
+		m 
 }
